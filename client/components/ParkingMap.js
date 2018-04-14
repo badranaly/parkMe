c2cd5ea9fc1c304f5148c91a7cb2984da9759ae9
Aly Badran <aly2cool@hotmail.com>
2018-04-14 06:06:58 -0400

    got markers to render on map

--- client/components/ParkingMap.js
+++ client/components/ParkingMap.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-import { View, AppRegistry } from 'react-native';
+import { View, AppRegistry, Text, StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
+// import Marker from 'react-native-maps'
+
+// const LatLng = {latitude: -36.8457991, longitude: 174.7666099}
 
 export default class sandbox extends Component {
   render() {
     return (
-      <View style={{ position: 'relative', height: 500}}>
-        <MapView
-          style={{ left:0, right: 0, top:0, bottom: 0, position: 'absolute' }}
+      <View style={styles.container}>
+        <MapView style={styles.map}
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0,
+              longitudeDelta: 0.0,
           }}
-        />
-      </View>
+        >
+        <MapView.Marker
+            coordinate={{latitude: 37.78825, longitude: -122.4324}}
+            title={"User"}
+            description={"leaving my parking spot"}
+         />
+       </MapView>
+    </View>
 
     );
   }
 }
+
+var styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+    map: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    }
+});
