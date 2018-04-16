97161e77b67f8892379890d09eb89e15f87d0563
Aly Badran <aly2cool@hotmail.com>
2018-04-16 03:18:26 -0400

    configured for deployment

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,5 +1,7 @@
-module.exports = {
-  host: 'localhost',
-  port: 5432,
-  database: 'parkme_db'
-}
+const config = process.env.DATABASE_URL || {
+  host: process.env.PG_HOST || 'localhost',
+  port: process.env.PG_PORT || '5432',
+  database: process.env.PG_DATABASE || 'parkme_db',
+};
+
+module.exports = config;
